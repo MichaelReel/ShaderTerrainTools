97703623b8804af23a9aed206781e6f970586856
Michael Reel <mykilr@gmail.com>
2022-10-30 22:06:26 +0000

    Using the last flood, speeds up fill, but the fill is still too slow

--- TerrainBuilder.gd
+++ TerrainBuilder.gd
@@ -1,21 +1,22 @@
 extends Control
 
-const layers : int = 16 # 256
+const layers : int = 256 # 256
 const slice_depth : float = 1.0 / float(layers)
 
 var terrain_workspace : TextureArray
 var terrain_layer : int = 0
 var flood_repeat_hash : String = ""
+var flood_last_layer : ImageTexture
 
-onready var slice_viewport := $SliceViewport
-onready var slice_texture := $SliceViewport/TextureRect
-onready var slice_timer := $SliceTimer
-onready var slice_display := $DisplaySlice
+onready var slice_viewport := $Slice/SliceViewport
+onready var slice_texture := $Slice/SliceViewport/TextureRect
+onready var slice_timer := $Slice/SliceTimer
+onready var slice_display := $Slice/DisplaySlice
 
-onready var flood_viewport := $FloodViewport
-onready var flood_texture := $FloodViewport/TextureRect
-onready var flood_timer := $FloodTimer
-onready var flood_display := $DisplayFlood
+onready var flood_viewport := $Flood/FloodViewport
+onready var flood_texture := $Flood/FloodViewport/TextureRect
+onready var flood_timer := $Flood/FloodTimer
+onready var flood_display := $Flood/DisplayFlood
 
 onready var debug_texture := $Debug/Viewport/TextureRect
 onready var debug_timer := $Debug/DebugTimer
@@ -25,13 +26,15 @@ onready var debug_display := $Debug/DisplayDebug
 func _ready() -> void:
 	_setup_slicing()
 
+### SLICING ###
+
 func _setup_slicing() -> void:
 	terrain_workspace = TextureArray.new()
-	terrain_workspace.create(slice_viewport.size.x, slice_viewport.size.y, 256, Image.FORMAT_RGBA8)
+	terrain_workspace.create(slice_viewport.size.x, slice_viewport.size.y, layers, Image.FORMAT_RGBA8)
 	slice_texture.material.set_shader_param("slice_height", slice_depth * float(terrain_layer))
 	slice_timer.start()
 
-func _on_SliceTimer_timeout()-> void:
+func _on_SliceTimer_timeout() -> void:
 	slice_viewport.set_clear_mode(Viewport.CLEAR_MODE_ONLY_NEXT_FRAME)
 	yield(VisualServer, "frame_post_draw")
 	var img : Image = slice_viewport.get_texture().get_data()
@@ -46,6 +49,8 @@ func _on_SliceTimer_timeout()-> void:
 	
 	slice_texture.material.set_shader_param("slice_height", slice_depth * float(terrain_layer))
 
+### FLOODING ###
+
 func _hash_image(img : Image) -> String:
 	var ctx = HashingContext.new()
 	ctx.start(HashingContext.HASH_SHA256)
@@ -58,6 +63,9 @@ func _setup_flooding() -> void:
 	var texture := ImageTexture.new()
 	texture.create_from_image(terrain_workspace.get_layer_data(terrain_layer))
 	flood_texture.texture = texture
+	
+	flood_texture.material.set_shader_param("last_layer", texture)
+	
 	flood_timer.start()
 	slice_display.visible = false
 	flood_display.visible = true
@@ -71,6 +79,9 @@ func _on_FloodTimer_timeout() -> void:
 	if hash_code == flood_repeat_hash:
 		# store this result in the terrain workspace
 		terrain_workspace.set_layer_data(img, terrain_layer)
+		var flood_last_layer := ImageTexture.new()
+		flood_last_layer.create_from_image(img)
+		flood_texture.material.set_shader_param("last_layer", flood_last_layer)
 		
 		# If last layer, stop the timer and move to the next stage
 		terrain_layer += 1
@@ -88,21 +99,30 @@ func _on_FloodTimer_timeout() -> void:
 	var texture = ImageTexture.new()
 	texture.create_from_image(img)
 	flood_texture.texture = texture
-	
+
+### DEBUG ###
+
 func _setup_debug() -> void:
 	terrain_layer = 0
 	debug_timer.start()
+	var img := terrain_workspace.get_layer_data(terrain_layer)
+	var texture := ImageTexture.new()
+	texture.create_from_image(img)
+	debug_texture.texture = texture
+	print("debug layer " + str(terrain_layer))
 	slice_display.visible = false
 	flood_display.visible = false
 	debug_display.visible = true
 
 func _on_DebugTimer_timeout() -> void:
-	var img := terrain_workspace.get_layer_data(terrain_layer)
-	var texture := ImageTexture.new()
-	texture.create_from_image(img)
-	debug_texture.texture = texture
 	terrain_layer += 1
 	if terrain_layer >= layers:
 		debug_timer.stop()
 		return
+		
+	print("debug layer " + str(terrain_layer))
+	var img := terrain_workspace.get_layer_data(terrain_layer)
+	var texture := ImageTexture.new()
+	texture.create_from_image(img)
+	debug_texture.texture = texture
 	
