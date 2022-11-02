5e92c5d54a3b2ac741e08e62cf5548033d666394
Michael Reel <mykilr@gmail.com>
2022-11-02 23:31:08 +0000

    Added some more vars to the terrain gen

--- tool/terrain/TerrainMesh.gd
+++ tool/terrain/TerrainMesh.gd
@@ -10,9 +10,20 @@ onready var slider_xshape := $GridContainer/HSlider_XShape
 onready var slider_depth := $GridContainer/HSlider_Depth
 onready var slider_min_range := $GridContainer/HSlider_Min_Range
 onready var slider_max_range := $GridContainer/HSlider_Max_Range
+onready var slider_root_offset_x := $GridContainer/HSlider_RootOffset_X
+onready var slider_root_offset_y := $GridContainer/HSlider_RootOffset_Y
+onready var slider_warp_offset_1_x := $GridContainer/HSlider_WarpOffset_X_1
+onready var slider_warp_offset_1_y := $GridContainer/HSlider_WarpOffset_Y_1
+onready var slider_warp_offset_2_x := $GridContainer/HSlider_WarpOffset_X_2
+onready var slider_warp_offset_2_y := $GridContainer/HSlider_WarpOffset_Y_2
 onready var debounce_timer := $GridContainer/DebounceTimer
 
+onready var rotation_player := $TerrainViewportContainer/Viewport/AnimationPlayer
+
+var rotation_speed = -0.1;
+
 func _ready() -> void:
+	rotation_player.play("rotate", -1, rotation_speed)
 	_update_texture_shader_uniforms()
 	_update_terrain_shader_uniforms()
 
@@ -34,6 +45,16 @@ func _update_texture_shader_uniforms() -> void:
 	texture_rect.material.set_shader_param("depth", slider_depth.value)
 	texture_rect.material.set_shader_param("min_range", slider_min_range.value)
 	texture_rect.material.set_shader_param("max_range", slider_max_range.value)
+	var root_offset = Vector2(slider_root_offset_x.value, slider_root_offset_y.value)
+	var warp_offset_1 = Vector2(slider_warp_offset_1_x.value, slider_warp_offset_1_y.value)
+	var warp_offset_2 = Vector2(slider_warp_offset_2_x.value, slider_warp_offset_2_y.value)
+	texture_rect.material.set_shader_param("root_offset", root_offset)
+	texture_rect.material.set_shader_param("warp_offset_1", warp_offset_1)
+	texture_rect.material.set_shader_param("warp_offset_2", warp_offset_2)
+
+# uniform vec2 root_offset = vec2(0.0, 0.0);
+# uniform vec2 warp_offset_1 = vec2(0.0, 0.3);
+# uniform vec2 warp_offset_2 = vec2(5.2, 1.3);
 
 
 func _on_HSlider_drag_ended(value_changed: bool) -> void:
